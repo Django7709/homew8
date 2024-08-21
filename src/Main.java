0f221f2f259f882cd418732244d56fd869d08c19
django <pitbull.09@mail.ru>
2024-08-21 20:51:06 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i + " ");
         }
-    //задача 2
+        //задача 2
         for (int i = 10; i > 1; i--) {
             System.out.println(i + " ");
         }
@@ -24,30 +24,31 @@ public class Main {
             System.out.println(year + " год является високосным ");
         }
         // задача 6
-        for (int i = 7; i <= 98; i+= 7) {
+        for (int i = 7; i <= 98; i += 7) {
             System.out.println(i + " ");
         }
         //задача 7
-        for (int i = 1; i <= 512; i*= 2) {
+        for (int i = 1; i <= 512; i *= 2) {
             System.out.println(i + " ");
         }
         // задача 8
-    int amount = 29000;
+        int amount = 29000;
         int sum = 0;
-        for ( int month = 1; month <= 12; month ++ ) {
-        sum += amount;
-        System.out.println(" месяц " + month + " сумма накоплений равна" + sum + " рублей ");
+        for (int month = 1; month <= 12; month++) {
+            sum += amount;
+            System.out.println(" месяц " + month + " сумма накоплений равна" + sum + " рублей ");
         }
-    // задача 9
+        // задача 9
         sum = 0;
         double percent = 10 / 100;
-        for ( int month = 1; month <= 12; month ++ ) {
+        for (int month = 1; month <= 12; month++) {
             sum += amount;
             sum = (int) (sum * (1 + percent));
             System.out.println(" месяц " + month + " сумма накоплений равна" + sum + " рублей ");
         }
         // задача 10
-    for ( int multiplier =1; multiplier <= 10; multiplier ++ ) {
-    System.out.printlf( " %d x %d = %d%n", 2, multiplier, 2* multiplier );
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            System.out.printlf(" %d x %d = %d%n", 2, multiplier, 2 * multiplier);
+        }
     }
-    }}
+}
